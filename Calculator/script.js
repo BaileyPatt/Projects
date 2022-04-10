66659a4fc7a78392f4b2e339264afb200ada3fb0
BaileyPatt <bpatt@xpanxion.com>
2022-04-10 19:59:19 -0400

    First Commit

---
+++ Calculator/script.js
@@ -0,0 +1,20 @@
+let display = document.getElementById('index')
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
